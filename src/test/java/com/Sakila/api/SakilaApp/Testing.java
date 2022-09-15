cc50adab160e12199e160613b84206f119ee71de
Wentong Wu <wuwentong2000@gmail.com>
2022-09-15 15:02:04 +0100

    Test all category

--- src/test/java/com/Sakila/api/SakilaApp/Testing.java
+++ src/test/java/com/Sakila/api/SakilaApp/Testing.java
@@ -1,5 +1,6 @@
 package com.Sakila.api.SakilaApp;
 
+import org.checkerframework.checker.units.qual.C;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -32,21 +33,28 @@ public class Testing {
     @Test
     void TestAllFilms(){
         List<Film> filmList = new ArrayList<>();
-
         Film testFilm = new Film(10,"WOW","POG",1,20.06,32,12.45,"wo");
         Film testFilm2 = new Film(11,"WOW","POG",1,20.06,32,12.45,"wo");
         filmList.add(testFilm);
         filmList.add(testFilm2);
-
         Iterable<Film> filmIterable = filmList;
-
         when(filmRepository.findAll()).thenReturn(filmIterable);
-
         Iterable<Film> Expected = filmIterable;
         Iterable<Film> Actual = sakilaAppApplication.getAllFilms();
-
-        Assertions.assertEquals(Expected, Actual, "Get all films");
-
+        Assertions.assertEquals(Expected, Actual, "Error");
+    }
+    @Test
+    void TestAllCategory(){
+        List<Category> categoryList = new ArrayList<>();
+        Category testCategory = new Category(10,"a");
+        Category testCategory2 = new Category(11,"b");
+        categoryList.add(testCategory);
+        categoryList.add(testCategory2);
+        Iterable<Category> categoryIterable = categoryList;
+        when(categoryRepository.findAll()).thenReturn(categoryIterable);
+        Iterable<Category> Expected = categoryIterable;
+        Iterable<Category> Actual = sakilaAppApplication.getAllCategory();
+        Assertions.assertEquals(Expected, Actual, "Error");
     }
 
 }
