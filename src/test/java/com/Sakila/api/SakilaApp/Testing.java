f80f71df955d01e266c7d02204dc9dbf7c7ee4c9
Wentong Wu <wuwentong2000@gmail.com>
2022-09-15 16:36:21 +0100

    Test getter and setter for film

--- src/test/java/com/Sakila/api/SakilaApp/Testing.java
+++ src/test/java/com/Sakila/api/SakilaApp/Testing.java
@@ -13,6 +13,8 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -65,6 +67,49 @@ public class Testing {
         sakilaAppApplication.deleteFilm(10);
         verify(filmRepository).deleteById(testFilm.getFilm_id());
         //Test to see if the function is being called. Does not test the data to see if it is actually deleted.
+    }
+    @Test
+    void TestGettersFilmID(){
+        Film testFilm = new Film(103,"WOW","POG",1,20.06,32,12.45,"wo");
+        Assertions.assertEquals(103,testFilm.getFilm_id(),"Test 'getFilm_id' failed");
+        Assertions.assertEquals("WOW",testFilm.getTitle(),"Test 'getTitle' failed");
+        Assertions.assertEquals("POG",testFilm.getDescription(),"Test 'getDescription' failed");
+        Assertions.assertEquals(1,testFilm.getRental_duration(),"Test 'getRental_duration' failed");
+        Assertions.assertEquals(20.06,testFilm.getRental_rate(),"Test 'getRental_rate' failed");
+        Assertions.assertEquals(32,testFilm.getLength(),"Test 'getLength' failed");
+        Assertions.assertEquals(12.45,testFilm.getReplacement_cost(),"Test 'getReplacement_cost' failed");
+        Assertions.assertEquals("wo",testFilm.getRating(),"Test 'getRating' failed");
+    }
+    @Test
+    void TestSetterFilmID(){
+        Film testFilm = new Film();
+        testFilm.setFilm_id(1);
+        testFilm.setTitle("Smile");
+        testFilm.setDescription("I am a Joke");
+        testFilm.setRental_duration(9);
+        testFilm.setRental_rate(10.01);
+        testFilm.setLength(102);
+        testFilm.setReplacement_cost(34.20);
+        testFilm.setRating("Spinner");
+        Assertions.assertEquals(1,testFilm.getFilm_id(),"Test 'setFilm_id' failed");
+        Assertions.assertEquals("Smile",testFilm.getTitle(),"Test 'setTitle' failed");
+        Assertions.assertEquals("I am a Joke",testFilm.getDescription(),"Test 'setDescription' failed");
+        Assertions.assertEquals(9,testFilm.getRental_duration(),"Test 'setRental_duration' failed");
+        Assertions.assertEquals(10.01,testFilm.getRental_rate(),"Test 'setRental_rate' failed");
+        Assertions.assertEquals(102,testFilm.getLength(),"Test 'setLength' failed");
+        Assertions.assertEquals(34.20,testFilm.getReplacement_cost(),"Test 'setReplacement_cost' failed");
+        Assertions.assertEquals("Spinner",testFilm.getRating(),"Test 'setRating' failed");
+    }
+
+    @Test
+    void AddFilm(){
+        Film testFilm = new Film(101,"WOW","POG",1,20.06,32,12.45,"wo");
+        sakilaAppApplication.addFilm(testFilm);
+        when(filmRepository.findById(testFilm.film_id)).thenReturn(Optional.of(testFilm));
+        Optional<Film> optional = sakilaAppApplication.getFilmByID(testFilm.film_id);
+        Film actual = optional.get();
+        Film expected = testFilm;
+        Assertions.assertEquals(expected,actual,"Testing add method");
     }
     @Test
     void TestAllCategory(){
